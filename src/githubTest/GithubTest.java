be1304f6c8d8db2f8226b61d25d4bcaae717266d
Saudade Dark <saudadedark@gmail.com>
2021-08-04 16:46:50 +0200

    new line

--- src/githubTest/GithubTest.java
+++ src/githubTest/GithubTest.java
@@ -5,6 +5,7 @@ public class GithubTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Peach!");
+		System.out.println("You are awesome");
 	}
 
 }
